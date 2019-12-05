e07ad970274476165e674dc4fadc0a2f5ddeedd1
Neil Mosunic <neilm813@gmail.com>
2019-12-04 16:17:22 -0800

    __str__

--- week2/d3/server/shirts/models.py
+++ week2/d3/server/shirts/models.py
@@ -8,3 +8,11 @@ class User(models.Model):
     password = models.CharField(max_length=60)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # this returned string is what will be shown when printing this user
+    def __str__(self):
+        s = '\n'
+        s += f"first_name: {self.first_name}\n"
+        s += f"last_name: {self.last_name}\n"
+        s += f"last_name: {self.email}\n"
+        return s
